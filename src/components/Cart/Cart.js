fda0a2c5260fc5433d9da228056834dd9d0b6cbf
john-roufaeil <john.f.roufaeil@gmail.com>
2024-10-01 00:08:18 +0300

    feat: finish repeated components

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Cart.css';
+import logo from "../../assets/logo.png";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Features from '../Features/Features';
 
 const Cart = () => (
-  <div className="Cart">
-    Cart Component
-  </div>
+    <div className="Cart">
+        <div className="title-bg w-full h-fit py-24 flex flex-col items-center gap-4">
+            <img src={logo} alt="logo" className="w-12" />
+            <h1 className="text-4xl font-semibold">Cart</h1>
+            <p>Home <ArrowForwardIosIcon fontSize='small' /> Cart</p>
+        </div>
+        <Features />
+    </div>
 );
-
-Cart.propTypes = {};
-
-Cart.defaultProps = {};
 
 export default Cart;
--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Features.css';
+import trophy from '../../assets/trophy.png';
+import warranty from '../../assets/warranty.png';
+import shipping from '../../assets/shipping.png';
+import customer from '../../assets/customer-support.png';
 
 const Features = () => (
-  <div className="Features">
-    Features Component
-  </div>
+    <div className="Features bg-accent-bg w-full flex justify-evenly py-16">
+        <div className="flex align-center gap-2">
+            <img src={trophy} className="w-12 h-12" alt="trophy" />
+            <div className="flex flex-col">
+                <p className="text-lg text-left font-semibold">High Quality</p>
+                <p className="text-subtext text-left font-semibold">Crafted from top materials</p>
+            </div>
+        </div>
+        <div className="flex align-center gap-2">
+            <img src={warranty} className="w-12 h-12" alt="trophy" />
+            <div className="flex flex-col">
+                <p className="text-lg text-left font-semibold">Warranty Protection</p>
+                <p className="text-subtext text-left font-semibold">Over 2 years</p>
+            </div>
+        </div>
+        <div className="flex align-center gap-2">
+            <img src={shipping} className="w-12 h-12" alt="trophy" />
+            <div className="flex flex-col">
+                <p className="text-lg text-left font-semibold">Free Shipping</p>
+                <p className="text-subtext text-left font-semibold">Order over $150</p>
+            </div>
+        </div>
+        <div className="flex align-center gap-2">
+            <img src={customer} className="w-12 h-12" alt="trophy" />
+            <div className="flex flex-col">
+                <p className="text-lg text-left font-semibold">24/7 Support</p>
+                <p className="text-subtext text-left font-semibold">Dedicated support</p>
+            </div>
+        </div>
+    </div>
 );
-
-Features.propTypes = {};
-
-Features.defaultProps = {};
 
 export default Features;
